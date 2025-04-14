a5e665b89b6df97dd73eb2809345ca0cd78e2563
[AxelFONTEYNE] <[axfonteyne3@gmail.com]>
2025-04-14 11:58:58 +0200

    BoundaryJeu

---
+++ src/controllers/ControlJeu.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import pirate.Pirate;
+
+public class ControlJeu {
+	public void setJoueur1(String nom, Pirate pirate) {
+		
+	}
+	
+	public void setJoueur2(String nom, Pirate pirate) {
+		
+	}
+}
