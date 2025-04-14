3a27af1de3335d19556a1eb498ec093a8e2950ef
aymericfrcs <114427130+aymericfrcs@users.noreply.github.com>
2025-04-14 09:56:06 +0200

    ajout ControlJeu

---
+++ src/controllers/ControlJeu.java
@@ -0,0 +1,27 @@
+package controllers;
+
+public class ControlJeu {
+
+	//private Jeu jeu;
+	
+	private ControlJoueur cJ1;
+	private ControlJoueur cJ2;
+	private ControlPioche cPioche;
+	//private ControlMarche cMarche;
+	private ControlCartePlateau cCartePlateau;
+	
+	
+	public ControlJeu() {
+		
+	}
+	
+	public void initJeu() {
+		
+		initJoueurs();
+		
+	}
+	
+	public void initJoueurs() {
+		
+	}
+}
